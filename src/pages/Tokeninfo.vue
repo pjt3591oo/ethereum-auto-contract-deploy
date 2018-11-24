5d66d0b562c2b972ce604bd52c06b1ed021ed864
pjt3591oo <pjt3591oo@gmail.com>
2018-11-24 23:57:51 +0900

    토큰 전송기능을 추가한다

--- src/pages/Tokeninfo.vue
+++ src/pages/Tokeninfo.vue
@@ -17,7 +17,16 @@
       <div class="title">
         <h2>토큰정보</h2>
       </div>
-
+      <v-text-field
+        v-model="userToken"
+        label="보유중인 토큰"
+        required
+      >
+        <v-tooltip slot="append" bottom>
+          <v-icon slot="activator" color="#b3d4fc">help</v-icon>
+          <span>해당 유저가 보유중인 토큰</span>
+        </v-tooltip>
+      </v-text-field>
       <v-text-field
         v-model="decimal"
         label="decimal"
@@ -60,6 +69,33 @@
           <span>토큰전체 발행량</span>
         </v-tooltip>
       </v-text-field>
+
+      <div class="title">
+        <h2>토큰전송</h2>
+      </div>
+
+      <v-text-field
+        v-model="transferValue"
+        label="transferValue"
+        required
+      >
+        <v-tooltip slot="append" bottom>
+          <v-icon slot="activator" color="#b3d4fc">help</v-icon>
+          <span>토큰전송량</span>
+        </v-tooltip>
+      </v-text-field>
+      <v-text-field
+        v-model="transferTo"
+        label="transferTo"
+        required
+      >
+        <v-tooltip slot="append" bottom>
+          <v-icon slot="activator" color="#b3d4fc">help</v-icon>
+          <span>토큰 받는 계정주소</span>
+        </v-tooltip>
+      </v-text-field>
+
+      <v-btn @click="transfer()">transfer</v-btn>
     </div>
   </div>
 </template>
@@ -73,11 +109,14 @@ export default {
   data () {
     return {
       tokenContractAddress: '0xf825da5d575cc635a1cbcc54d2b3edd9d882b6d1',
+      userToken: '', // 해당 유저가 보유중인 토큰
       decimal: '',
       tokenName: '',
       symbol: '',
       totalSupply: '',
-      abi: tokenAbi()
+      abi: tokenAbi(),
+      transferValue: '', // 토큰 전송량
+      transferTo: '' // 토큰 받는 계정주소
     }
   },
   methods: {
@@ -89,6 +128,10 @@ export default {
       let Courses = CoursesContract.at(this.tokenContractAddress)
       let self = this
 
+      Courses.balanceOf(web3.eth.defaultAccount, function (error, userToken) {
+        console.log(error)
+        self.userToken = userToken
+      })
       Courses.name(function (error, tokenName) {
         console.log(error)
         self.tokenName = tokenName
@@ -105,6 +148,18 @@ export default {
         console.log(error)
         self.totalSupply = totalSupply
       })
+    },
+    transfer () {
+      let web3 = new Web3(window.web3.currentProvider)
+      web3.eth.defaultAccount = web3.eth.accounts[0]
+
+      let CoursesContract = web3.eth.contract(this.abi)
+      let Courses = CoursesContract.at(this.tokenContractAddress)
+      console.log(this.transferTo)
+      console.log(this.transferValue)
+      Courses.transfer(this.transferTo, this.transferValue, function (err, data) {
+        console.log(data)
+      })
     }
   }
 }
@@ -120,6 +175,7 @@ export default {
 }
 .title {
   text-align: left;
+  margin: 20px 0 20px 0;
 }
 h2 {
   color: rgb(125, 125, 125);
