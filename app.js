86ee96d6ba7b14ea1103e32d045f4ac71d2c3c60
pjt3591oo <pjt3591oo@gmail.com>
2018-11-25 10:03:45 +0900

    PORT 번호를 수정한다

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', (req, res, next) => {
     })
 })
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var server = app.listen(3001, function(){
+    console.log("Express server has started on port 3001")
 })
