2f7350d859591af3619586ff5d8390b899b01733
pjt3591oo <pjt3591oo@gmail.com>
2018-11-25 09:55:52 +0900

    Home.vue에서 각 페이지 이동 가이드를 추가한다

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home-page">
     <h1>{{ msg }}</h1>
+
+    <div class="hot-key">
+      <div>
+        <h2>
+          <router-link to="/deploy">Token 배포하러 가기</router-link>
+        </h2>
+      </div>
+      <div>
+        <h2>
+          <router-link to="/tokeninfo">Token 관리하러 가기</router-link>
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +32,16 @@ export default {
 h1 {
   font-weight: normal;
   color: $c-primary;
+}
+a {
+  text-decoration: none;
+}
+
+.hot-key {
+  margin-top: 50px;
+}
+.hot-key a{
+  margin-top: 20px;
+  color: hotpink;
 }
 </style>
